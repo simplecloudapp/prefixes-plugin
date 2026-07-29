583da8e2ae51afb2d5977a49ec8ae8ea6a773aa7
fllipeis <p.eistrach@gmail.com>
2026-07-29 20:37:39 +0200

    Remove outdated game version entries from build script

--- prefixes-paper/build.gradle.kts
+++ prefixes-paper/build.gradle.kts
@@ -43,21 +43,6 @@ modrinth {
     versionType.set("release")
     uploadFile.set(tasks.shadowJar)
     gameVersions.addAll(
-        "1.21",
-        "1.21.1",
-        "1.21.2",
-        "1.21.3",
-        "1.21.4",
-        "1.21.5",
-        "1.21.6",
-        "1.21.7",
-        "1.21.8",
-        "1.21.9",
-        "1.21.10",
-        "1.21.11",
-        "26.1",
-        "26.1.1",
-        "26.1.2",
         "26.2"
     )
     loaders.addAll("paper", "purpur")
